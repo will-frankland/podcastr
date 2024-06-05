99394f673fc65b39e3f003671b57f785ef236504
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-06-05 12:03:24 -0400

    setup styling and active classname for leftSidebar

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -1,9 +1,15 @@
+"use client";
 import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 
 const LeftSidebar = () => {
+  const pathname = usePathname();
+  const router = useRouter();
+
   return (
     <section className="left_sidebar">
       <nav className="flex flex-col gap-6">
@@ -16,11 +22,22 @@ const LeftSidebar = () => {
             Podcastr
           </h1>
         </Link>
-        {sidebarLinks.map(({ route, label }) => (
-          <Link href={route}>
-            {label}
-          </Link>
-        ))}
+        {sidebarLinks.map(({ route, label, imgURL }) => {
+          const isActive =
+            pathname === route || pathname.startsWith(`${route}/`);
+          return (
+            <Link
+              href={route}
+              key={label}
+              className={cn(
+                "flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start", { 'bg-nav-focus border-r-4 border-orange-1': isActive }
+              )}
+            >
+              <Image src={imgURL} alt={label} width={24} height={24} />
+              <p>{label}</p>
+            </Link>
+          );
+        })}
       </nav>
     </section>
   );
