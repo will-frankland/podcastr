875a1ee9851749eb836fbf1b452cbdd2e8851703
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-06-06 11:42:54 -0400

    setup and integrate convex with test data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import ConvexClientProvider from "./providers/ConvexProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,8 +9,8 @@ export const metadata: Metadata = {
   title: "Podcastr",
   description: "Generate your podcasts using AI",
   icons: {
-    icon: '/icons/logo.svg'
-  }
+    icon: "/icons/logo.svg",
+  },
 };
 
 export default function RootLayout({
@@ -19,7 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ConvexClientProvider>{children}</ConvexClientProvider>
+      </body>
     </html>
   );
 }
