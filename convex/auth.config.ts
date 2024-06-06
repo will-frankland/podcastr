fc65b8be467df1a87c47422353b1ceb7181f07c2
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-06-06 12:59:20 -0400

    setup clerk with convex

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://thankful-snail-23.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig
