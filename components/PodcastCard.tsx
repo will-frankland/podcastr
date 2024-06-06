2dc7602b0f3a20c6dc1fad7c24b759a3e3bd167a
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-06-06 11:27:29 -0400

    finalize podcast card

--- components/PodcastCard.tsx
+++ components/PodcastCard.tsx
@@ -16,6 +16,12 @@ const PodcastCard = ({
     <div className="cursor-pointer">
       <figure className="flex flex-col gap-2">
         <Image src={imgUrl} width={174} height={174} alt={title} />
+        <div className="flex flex-col">
+          <h1 className="text-16 truncate font-bold text-white-1">{title}</h1>
+          <h2 className="text-12 truncate font-normal capitalize text-white-4">
+            {description}
+          </h2>
+        </div>
       </figure>
     </div>
   );
