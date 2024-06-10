7ec1b81e538ed39163d6441fed561916d6cfd250
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-06-10 13:50:29 -0400

    create initial signup/signin pages and style

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,21 @@
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-      <main>
-        {children}
-      </main>
+    <main className="relative h-screen w-full">
+      <div className="absolute size-full">
+        <Image
+          src="/images/bg-img.png"
+          alt="background"
+          fill
+          className="size-full"
+        />
+      </div>
+      {children}
+    </main>
   );
 }
